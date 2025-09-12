53b61cd7b63126dc849b384db6029ac9bf5a94c4
YuCat <66810215+YuCat-OVO@users.noreply.github.com>
2025-09-13 00:02:20 +0800

    ✨ feat(startwm): 新增默认窗口管理器启动脚本
    - 添加Nvidia GPU支持检测和配置
    - 设置默认分辨率并实现分辨率自适应调整
    - 集成openbox会话启动

---
+++ root/defaults/startwm.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Enable Nvidia GPU support if detected
+if which nvidia-smi; then
+  export LIBGL_KOPPER_DRI2=1
+  export MESA_LOADER_DRIVER_OVERRIDE=zink
+  export GALLIUM_DRIVER=zink
+fi
+
+# Use a default resolution if unset
+if [ -z ${RESOLUTION+x} ]; then
+  RESOLUTION="1024x768"
+fi
+if [[ ${RESOLUTION} != "1024x768" ]]; then
+  sudo sed -i 's/resize=remote/resize=scale/g' /kclient/public/index.html
+fi
+/usr/bin/openbox-session
